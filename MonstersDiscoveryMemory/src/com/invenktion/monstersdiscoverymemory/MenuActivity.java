f35cc2084447011c887ee2015b391c9d2fd5d96e
mub <marcouberti84@gmail.com>
2013-11-06 20:30:44 +0100

    New graphics by A.Grilli. Image resources for small device. TODO multiscreen support with resourses and @dimen.

--- MonstersDiscoveryMemory/src/com/invenktion/monstersdiscoverymemory/MenuActivity.java
+++ MonstersDiscoveryMemory/src/com/invenktion/monstersdiscoverymemory/MenuActivity.java
@@ -1,7 +1,5 @@
 package com.invenktion.monstersdiscoverymemory;
 
-import com.google.android.gms.plus.PlusClient;
-import com.google.android.gms.plus.PlusOneButton;
 import com.invenktion.monstersdiscoverymemory.core.ActivityHelper;
 import com.invenktion.monstersdiscoverymemory.core.AnimationFactory;
 import com.invenktion.monstersdiscoverymemory.core.ApplicationManager;
@@ -12,57 +10,25 @@ import com.invenktion.monstersdiscoverymemory.core.SoundManager;
 import android.app.Activity;
 import android.app.Dialog;
 import android.app.KeyguardManager;
-import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.IntentSender.SendIntentException;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
-import android.graphics.Typeface;
-import android.graphics.drawable.BitmapDrawable;
-import android.media.AudioManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Debug;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
-import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.view.animation.Animation;
-import android.view.animation.Animation.AnimationListener;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
-import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
-
-
-public class MenuActivity extends Activity implements
-ConnectionCallbacks, OnConnectionFailedListener{
+public class MenuActivity extends Activity {
 	//Typeface font; 
 	float DENSITY = 1.0f;
-	
-	private static final int REQUEST_CODE_RESOLVE_ERR = 9000;
 
-    private ProgressDialog mConnectionProgressDialog;
-    private PlusClient mPlusClient;
-    private ConnectionResult mConnectionResult;
-
-	PlusOneButton mPlusOneButton;
-	
 	BroadcastReceiver mReceiver;
 	
 	static final int DIALOG_EXIT_APPLICATION = 0;
@@ -164,16 +130,12 @@ ConnectionCallbacks, OnConnectionFailedListener{
 	    }
 	    return super.onKeyDown(keyCode, event);
 	}
-	// The request code must be 0 or greater.
-	private static final int PLUS_ONE_REQUEST_CODE = 0;
 
+	
 	@Override
 	protected void onResume() {
 		super.onResume();
-		// Aggiorna lo stato del pulsante +1 ogni volta che l'attività riceve lo stato attivo.
-		mPlusOneButton.initialize("https://play.google.com/store/apps/details?id=com.invenktion.monstersdiscovery&hl=it", PLUS_ONE_REQUEST_CODE);
-
-
+		
 		//Rilancio la musica se e solo se non è già attiva
 		//Questo ci permette di utilizzare la stessa traccia musicale tra Activity differenti, oltre
 		//al metodo presente nel onPause che controlla se siamo o no in background
@@ -219,28 +181,6 @@ ConnectionCallbacks, OnConnectionFailedListener{
 		return false;
 	}
 	
-	@Override
-    protected void onStart() {
-        super.onStart();
-        mPlusClient.connect();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mPlusClient.disconnect();
-    }
-
-    public void onConnected() {
-        String accountName = mPlusClient.getAccountName();
-        Toast.makeText(this, accountName + " is connected.", Toast.LENGTH_LONG).show();
-    }
-
-    public void onDisconnected() {
-        Log.d("", "disconnected");
-    }
-
-	
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -255,17 +195,6 @@ ConnectionCallbacks, OnConnectionFailedListener{
         
         setContentView(R.layout.home);
         
-        mPlusClient = new PlusClient.Builder(this, this, this)
-		        .setVisibleActivities("http://schemas.google.com/AddActivity", "http://schemas.google.com/BuyActivity")
-		        .build();
-		// Barra di avanzamento da visualizzare se l'errore di connessione non viene risolto.
-		mConnectionProgressDialog = new ProgressDialog(this);
-		mConnectionProgressDialog.setMessage("Signing in...");
-
-
-        mPlusOneButton = (PlusOneButton) findViewById(R.id.plus_one_button);
-
-        
         this.DENSITY = getApplicationContext().getResources().getDisplayMetrics().density;
         
         FrameLayout frameLayout = (FrameLayout)findViewById(R.id.homelayout);
@@ -275,7 +204,7 @@ ConnectionCallbacks, OnConnectionFailedListener{
         ImageView medioBtn = (ImageView)findViewById(R.id.textcinque);
         //TextView tutorial = (TextView)findViewById(R.id.textquattro);
         ImageView difficileBtn = (ImageView)findViewById(R.id.texttre);
-        ImageView impossibileBtn = (ImageView)findViewById(R.id.impossibile);
+        //ImageView impossibileBtn = (ImageView)findViewById(R.id.impossibile);
        
         facileBtn.setOnTouchListener(new OnTouchListener() {
 			public boolean onTouch(View v, MotionEvent event) {
@@ -326,6 +255,7 @@ ConnectionCallbacks, OnConnectionFailedListener{
 				return true;
 			}
 		});
+        /*
         impossibileBtn.setOnTouchListener(new OnTouchListener() {
 			
 			public boolean onTouch(View v, MotionEvent event) {
@@ -342,6 +272,7 @@ ConnectionCallbacks, OnConnectionFailedListener{
 				return true;
 			}
 		});
+		*/
         
         
         //FISSO LA DIMENSIONE DEI TRE BOTTONI IN MODO PERCENTUALE RISPETTO ALLO SCHERMO
@@ -352,13 +283,13 @@ ConnectionCallbacks, OnConnectionFailedListener{
         int W_PLAY = (int)((double)ApplicationManager.SCREEN_W / (double)2.5);
         int H_PLAY = (int)(W_PLAY / proporzioniPlay);
         
-        int W_BTN = (int)((double)ApplicationManager.SCREEN_W / (double)3);
+        int W_BTN = (int)((double)ApplicationManager.SCREEN_W / (double)2.5);
         int H_BTN = (int)(W_BTN / proporzioniAltriBtn);
         
         facileBtn.setLayoutParams(new LinearLayout.LayoutParams(W_PLAY,H_PLAY));
         medioBtn.setLayoutParams(new LinearLayout.LayoutParams(W_BTN,H_BTN));
         difficileBtn.setLayoutParams(new LinearLayout.LayoutParams(W_BTN,H_BTN));
-        impossibileBtn.setLayoutParams(new LinearLayout.LayoutParams(W_BTN,H_BTN));
+        //impossibileBtn.setLayoutParams(new LinearLayout.LayoutParams(W_BTN,H_BTN));
         
         final ImageView creditsImage = (ImageView)findViewById(R.id.creditsimage);
         creditsImage.setOnTouchListener(new OnTouchListener() {
@@ -433,41 +364,4 @@ ConnectionCallbacks, OnConnectionFailedListener{
         rotAnim.setFillAfter(true);rotAnim.setFillBefore(true);
         faceJhonny.setAnimation(rotAnim);
     }
-
-	@Override
-	public void onConnectionFailed(ConnectionResult result) {
-		if (mConnectionProgressDialog.isShowing()) {
-            // L'utente ha già fatto clic sul pulsante di accesso. Inizia a risolvere
-            // gli errori di connessione. Attendi fino a onConnected() per eliminare la
-            // finestra di dialogo di connessione.
-            if (result.hasResolution()) {
-                    try {
-                            result.startResolutionForResult(this, REQUEST_CODE_RESOLVE_ERR);
-                    } catch (SendIntentException e) {
-                            mPlusClient.connect();
-                    }
-            }
-    }
-
-    // Salva l'intent in modo che sia possibile avviare un'attività quando l'utente fa clic
-    // sul pulsante di accesso.
-    mConnectionResult = result;
-
-	}
-
-	@Override
-	public void onConnected(Bundle arg0) {
-		// Abbiamo risolto ogni errore di connessione.
-		  mConnectionProgressDialog.dismiss();
-
-	}
-
-	@Override
-	protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
-	    if (requestCode == REQUEST_CODE_RESOLVE_ERR && responseCode == RESULT_OK) {
-	        mConnectionResult = null;
-	        mPlusClient.connect();
-	    }
-	}
-
 }
